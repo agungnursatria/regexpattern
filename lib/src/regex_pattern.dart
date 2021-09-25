706070446282da0f9a71f067a2168425f06b393f
Agung <agungnursatria@gmail.com>
2021-09-25 12:31:24 +0700

    Migrate to null-safety

--- lib/src/regex_pattern.dart
+++ lib/src/regex_pattern.dart
@@ -6,10 +6,10 @@ class RegexPattern {
   /// Username regex
   /// Requires minimum 3 character
   /// Allowing "_" and "." in middle of name
-  static Pattern username = r'^[a-zA-Z0-9][a-zA-Z0-9_.]+[a-zA-Z0-9]$';
+  static String username = r'^[a-zA-Z0-9][a-zA-Z0-9_.]+[a-zA-Z0-9]$';
 
   /// Email regex
-  static Pattern email =
+  static String email =
       r'^[a-z0-9]+([-+._][a-z0-9]+){0,2}@.*?(\.(a(?:[cdefgilmnoqrstuwxz]|ero|(?:rp|si)a)|b(?:[abdefghijmnorstvwyz]iz)|c(?:[acdfghiklmnoruvxyz]|at|o(?:m|op))|d[ejkmoz]|e(?:[ceghrstu]|du)|f[ijkmor]|g(?:[abdefghilmnpqrstuwy]|ov)|h[kmnrtu]|i(?:[delmnoqrst]|n(?:fo|t))|j(?:[emop]|obs)|k[eghimnprwyz]|l[abcikrstuvy]|m(?:[acdeghklmnopqrstuvwxyz]|il|obi|useum)|n(?:[acefgilopruz]|ame|et)|o(?:m|rg)|p(?:[aefghklmnrstwy]|ro)|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t(?:[cdfghjklmnoprtvwz]|(?:rav)?el)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])\b){1,2}$';
 
   /// URL regex
@@ -17,165 +17,165 @@ class RegexPattern {
   /// - https://medium.com/@diegoveloper/flutter-widget-size-and-position-b0a9ffed9407
   /// - https://www.youtube.com/watch?v=COYFmbVEH0k
   /// - https://stackoverflow.com/questions/53913192/flutter-change-the-width-of-an-alertdialog/57688555
-  static Pattern url =
+  static String url =
       r"^((((H|h)(T|t)|(F|f))(T|t)(P|p)((S|s)?))\://)?(www.|[a-zA-Z0-9].)[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,6}(\:[0-9]{1,5})*(/($|[a-zA-Z0-9\.\,\;\?\'\\\+&amp;%\$#\=~_\-@]+))*$";
 
   /// Phone Number regex
   /// Must started by either, "0", "+", "+XX <X between 2 to 4 digit>", "(+XX <X between 2 to 3 digit>)"
   /// Can add whitespace separating digit with "+" or "(+XX)"
   /// Example: 05555555555, +555 5555555555, (+123) 5555555555, (555) 5555555555, +5555 5555555555
-  static Pattern phone =
+  static String phone =
       r'^(0|\+|(\+[0-9]{2,4}|\(\+?[0-9]{2,4}\)) ?)([0-9]*|\d{2,4}-\d{2,4}(-\d{2,4})?)$';
 
   /// Hexadecimal regex
-  static Pattern hexadecimal = r'^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$';
+  static String hexadecimal = r'^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$';
 
   /// Image vector regex
-  static Pattern vector = r'.(svg)$';
+  static String vector = r'.(svg)$';
 
   /// Image regex
-  static Pattern image = r'.(jpeg|jpg|gif|png|bmp)$';
+  static String image = r'.(jpeg|jpg|gif|png|bmp)$';
 
   /// Audio regex
-  static Pattern audio = r'.(mp3|wav|wma|amr|ogg)$';
+  static String audio = r'.(mp3|wav|wma|amr|ogg)$';
 
   /// Video regex
-  static Pattern video = r'.(mp4|avi|wmv|rmvb|mpg|mpeg|3gp)$';
+  static String video = r'.(mp4|avi|wmv|rmvb|mpg|mpeg|3gp)$';
 
   /// Txt regex
-  static Pattern txt = r'.txt$';
+  static String txt = r'.txt$';
 
   /// Document regex
-  static Pattern doc = r'.(doc|docx)$';
+  static String doc = r'.(doc|docx)$';
 
   /// Excel regex
-  static Pattern excel = r'.(xls|xlsx)$';
+  static String excel = r'.(xls|xlsx)$';
 
   /// PPT regex
-  static Pattern ppt = r'.(ppt|pptx)$';
+  static String ppt = r'.(ppt|pptx)$';
 
   /// Document regex
-  static Pattern apk = r'.apk$';
+  static String apk = r'.apk$';
 
   /// PDF regex
-  static Pattern pdf = r'.pdf$';
+  static String pdf = r'.pdf$';
 
   /// HTML regex
-  static Pattern html = r'.html$';
+  static String html = r'.html$';
 
   /// DateTime regex (UTC)
   /// Unformatted date time (UTC and Iso8601)
   /// Example: 2020-04-27 08:14:39.977, 2020-04-27T08:14:39.977, 2020-04-27 01:14:39.977Z
-  static Pattern basicDateTime =
+  static String basicDateTime =
       r'^\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}.\d{3}Z?$';
 
   /// Binary regex
   /// Consist only 0 & 1
-  static Pattern binary = r'^[0-1]*$';
+  static String binary = r'^[0-1]*$';
 
   /// MD5 regex
-  static Pattern md5 = r'^[a-f0-9]{32}$';
+  static String md5 = r'^[a-f0-9]{32}$';
 
   /// SHA1 regex
-  static Pattern sha1 =
+  static String sha1 =
       r'(([A-Fa-f0-9]{2}\:){19}[A-Fa-f0-9]{2}|[A-Fa-f0-9]{40})';
 
   /// SHA256 regex
-  static Pattern sha256 =
+  static String sha256 =
       r'([A-Fa-f0-9]{2}\:){31}[A-Fa-f0-9]{2}|[A-Fa-f0-9]{64}';
 
   /// SSN (Social Security Number) regex
-  static Pattern ssn =
+  static String ssn =
       r'^(?!0{3}|6{3}|9[0-9]{2})[0-9]{3}-?(?!0{2})[0-9]{2}-?(?!0{4})[0-9]{4}$';
 
   /// IPv4 regex
-  static Pattern ipv4 = r'^(?:(?:^|\.)(?:2(?:5[0-5]|[0-4]\d)|1?\d?\d)){4}$';
+  static String ipv4 = r'^(?:(?:^|\.)(?:2(?:5[0-5]|[0-4]\d)|1?\d?\d)){4}$';
 
   /// IPv6 regex
-  static Pattern ipv6 =
+  static String ipv6 =
       r'^((([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}:[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){5}:([0-9A-Fa-f]{1,4}:)?[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){4}:([0-9A-Fa-f]{1,4}:){0,2}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){3}:([0-9A-Fa-f]{1,4}:){0,3}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){2}:([0-9A-Fa-f]{1,4}:){0,4}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(([0-9A-Fa-f]{1,4}:){0,5}:((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(::([0-9A-Fa-f]{1,4}:){0,5}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|([0-9A-Fa-f]{1,4}::([0-9A-Fa-f]{1,4}:){0,5}[0-9A-Fa-f]{1,4})|(::([0-9A-Fa-f]{1,4}:){0,6}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){1,7}:))$';
 
   /// ISBN 10 & 13 regex
-  static Pattern isbn =
+  static String isbn =
       r'(ISBN(\-1[03])?[:]?[ ]?)?(([0-9Xx][- ]?){13}|([0-9Xx][- ]?){10})';
 
   /// Github repository regex
-  static Pattern github =
+  static String github =
       r'((git|ssh|http(s)?)|(git@[\w\.]+))(:(\/\/)?)([\w\.@\:/\-~]+)(\.git)(\/)?';
 
   /// Passport No. regex
-  static Pattern passport = r'^(?!^0+$)[a-zA-Z0-9]{6,9}$';
+  static String passport = r'^(?!^0+$)[a-zA-Z0-9]{6,9}$';
 
   /// Currency regex
-  static Pattern currency =
+  static String currency =
       r'^(S?\$|\₩|Rp|\¥|\€|\₹|\₽|fr|R$|R)?[ ]?[-]?([0-9]{1,3}[,.]([0-9]{3}[,.])*[0-9]{3}|[0-9]+)([,.][0-9]{1,2})?( ?(USD?|AUD|NZD|CAD|CHF|GBP|CNY|EUR|JPY|IDR|MXN|NOK|KRW|TRY|INR|RUB|BRL|ZAR|SGD|MYR))?$';
 
   /// Numeric Only regex (No Whitespace & Symbols)
-  static Pattern numericOnly = r'^\d+$';
+  static String numericOnly = r'^\d+$';
 
   /// Alphabet Only regex (No Whitespace & Symbols)
-  static Pattern alphabetOnly = r'^[a-zA-Z]+$';
+  static String alphabetOnly = r'^[a-zA-Z]+$';
 
   /// Password (Easy) Regex
   /// Allowing all character except 'whitespace'
   /// Minimum character: 8
-  static Pattern passwordEasy = r'^\S{8,}$';
+  static String passwordEasy = r'^\S{8,}$';
 
   /// Password (Easy) Regex
   /// Allowing all character
   /// Minimum character: 8
-  static Pattern passwordEasyAllowedWhitespace = r'^[\S ]{8,}$';
+  static String passwordEasyAllowedWhitespace = r'^[\S ]{8,}$';
 
   /// Password (Normal) Regex
   /// Allowing all character except 'whitespace'
   /// Must contains at least: 1 letter & 1 number
   /// Minimum character: 8
-  static Pattern passwordNormal1 = r'^(?=.*[A-Za-z])(?=.*\d)\S{8,}$';
+  static String passwordNormal1 = r'^(?=.*[A-Za-z])(?=.*\d)\S{8,}$';
 
   /// Password (Normal) Regex
   /// Allowing all character
   /// Must contains at least: 1 letter & 1 number
   /// Minimum character: 8
-  static Pattern passwordNormal1AllowedWhitespace =
+  static String passwordNormal1AllowedWhitespace =
       r'^(?=.*[A-Za-z])(?=.*\d)[\S ]{8,}$';
 
   /// Password (Normal) Regex
   /// Allowing LETTER and NUMBER only
   /// Must contains at least: 1 letter & 1 number
   /// Minimum character: 8
-  static Pattern passwordNormal2 = r'^(?=.*[A-Za-z])(?=.*\d)[a-zA-Z0-9]{8,}$';
+  static String passwordNormal2 = r'^(?=.*[A-Za-z])(?=.*\d)[a-zA-Z0-9]{8,}$';
 
   /// Password (Normal) Regex
   /// Allowing LETTER and NUMBER only
   /// Must contains: 1 letter & 1 number
   /// Minimum character: 8
-  static Pattern passwordNormal2AllowedWhitespace =
+  static String passwordNormal2AllowedWhitespace =
       r'^(?=.*[A-Za-z])(?=.*\d)[a-zA-Z0-9 ]{8,}$';
 
   /// Password (Normal) Regex
   /// Allowing all character except 'whitespace'
   /// Must contains at least: 1 uppercase letter, 1 lowecase letter & 1 number
   /// Minimum character: 8
-  static Pattern passwordNormal3 = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$';
+  static String passwordNormal3 = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$';
 
   /// Password (Normal) Regex
   /// Allowing all character
   /// Must contains at least: 1 uppercase letter, 1 lowecase letter & 1 number
   /// Minimum character: 8
-  static Pattern passwordNormal3AllowedWhitespace =
+  static String passwordNormal3AllowedWhitespace =
       r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\S ]{8,}$';
 
   /// Password (Hard) Regex
   /// Allowing all character except 'whitespace'
   /// Must contains at least: 1 uppercase letter, 1 lowecase letter, 1 number, & 1 special character (symbol)
   /// Minimum character: 8
-  static Pattern passwordHard =
+  static String passwordHard =
       r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])\S{8,}$';
 
   /// Password (Hard) Regex
   /// Allowing all character
   /// Must contains at least: 1 uppercase letter, 1 lowecase letter, 1 number, & 1 special character (symbol)
   /// Minimum character: 8
-  static Pattern passwordHardAllowedWhitespace =
+  static String passwordHardAllowedWhitespace =
       r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[\S ]{8,}$';
 }
--- lib/src/regex_validator.dart
+++ lib/src/regex_validator.dart
@@ -1,6 +1,6 @@
 /// Regex Validator
 class RegVal {
   /// Returns whether the pattern has a match in the string [input].
-  static bool hasMatch(String s, Pattern p) =>
-      (s == null) ? false : RegExp(p).hasMatch(s);
+  static bool hasMatch(String str, String pattern) =>
+      RegExp(pattern).hasMatch(str);
 }
