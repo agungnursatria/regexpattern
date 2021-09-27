363ca0f1dbf337986837abe0b994ca3cccf36d56
Agung <agungnursatria@gmail.com>
2021-09-27 08:51:07 +0700

    Set str variable in RegVal.hasMatch nullable

--- lib/src/regex_validator.dart
+++ lib/src/regex_validator.dart
@@ -1,6 +1,6 @@
 /// Regex Validator
 class RegVal {
   /// Returns whether the pattern has a match in the string [input].
-  static bool hasMatch(String str, String pattern) =>
-      RegExp(pattern).hasMatch(str);
+  static bool hasMatch(String? str, String pattern) =>
+      str == null ? false : RegExp(pattern).hasMatch(str);
 }
